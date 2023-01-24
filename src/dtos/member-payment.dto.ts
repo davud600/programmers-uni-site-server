6ff9e5c0f8978334f08f7e6c35b7ec3475a9a61f
davud <davudgorqi@gmail.com>
2023-01-24 17:43:47 +0100

    fix: fixed regex to be able to add spaces to username

--- src/dtos/member-payment.dto.ts
+++ src/dtos/member-payment.dto.ts
@@ -2,7 +2,7 @@ import { IsNumber, IsString, Matches } from 'class-validator';
 
 export class MemberPaymentDto {
   @IsString()
-  @Matches(/^[a-zA-Z0-9]+#[0-9]{4}$/, {
+  @Matches(/^[a-zA-Z0-9\ ._-]+#[0-9]{4,}$/, {
     message: 'Invalid Discord username format',
   })
   public discordUsername: string;
