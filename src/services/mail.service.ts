2fb6f4297bddf08520488a5b523f19ba1d3b411e
davud <davudgorqi@gmail.com>
2023-02-04 02:49:00 +0100

    feat: installed nodemailer, receives email from api endpoint

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,7 +1,26 @@
 import { ContactMail } from '@/interfaces/contact-mail.interface';
+import nodemailer from 'nodemailer';
+import { EMAIL_USER, EMAIL_PASSWORD } from '@/config';
 
 export default class MailService {
   public static async sendMail(mail: ContactMail): Promise<void> {
-    console.log(mail);
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: mail.email,
+      to: EMAIL_USER,
+      subject: `${mail.fullName}, ${mail.email}`,
+      text: mail.messageContent,
+    });
+
+    console.log('Message sent: %s', info.messageId);
   }
 }
